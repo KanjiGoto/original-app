f745481ac8b4e2bf98595ab84105b85d5677964a
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-10 05:52:40 +0000

    indexのルーテングミスを修正した

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # 一覧表示のためのルーティングを設定
-  get 'goals/index' => 'goals#index'
+  get 'goals' => 'goals#index'
   
 end
