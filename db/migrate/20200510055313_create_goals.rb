6711d36e76350f82bdb91fed372c3bdc57183c14
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-10 06:00:53 +0000

    goalモデルに４つのカラムを追加した

--- db/migrate/20200510055313_create_goals.rb
+++ db/migrate/20200510055313_create_goals.rb
@@ -1,6 +1,10 @@
 class CreateGoals < ActiveRecord::Migration[5.2]
   def change
     create_table :goals do |t|
+      t.string :name
+      t.text :goal
+      t.text :reason
+      t.text :future
 
       t.timestamps
     end
