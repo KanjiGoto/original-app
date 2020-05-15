18c2c99d22c582b0a45e52066ed80665f04247d9
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-15 08:27:07 +0000

    投稿するためのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   
   # 一覧表示のためのルーティングを設定
   get 'goals' => 'goals#index'
+  目標を投稿するためのルーティングを設定
+  get 'goals/new' => 'goals#new'
   # トップ画面のためのルーティングを設定
   root 'goals#top'
   # サイトの説明の画面を表示させるためのルーティング
   get 'pages/index' => "pages#index"
+  
   
 end
