f320c7ab13f5d944b916aeee9c258c14dbd31c2c
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-15 08:44:57 +0000

    投稿した目標を保存するためのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'goals' => 'goals#index'
   # 目標を投稿するためのルーティングを設定
   get 'goals/new' => 'goals#new'
+  # 投稿した目標を保存するためのルーティングを設定
+  post 'goals' => 'goals#create'
   # トップ画面のためのルーティングを設定
   root 'goals#top'
   # サイトの説明の画面を表示させるためのルーティング
