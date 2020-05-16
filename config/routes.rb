4c67d07e186823836d8ad61a39a259a15995088a
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-16 07:09:28 +0000

    投稿した目標を削除するためのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,21 @@ Rails.application.routes.draw do
   
   # 一覧表示のためのルーティングを設定
   get 'index' => 'goals#index'
-  # マイページを表示させるためのルーティング
-  get 'users/:id' => 'users#show'
   # 目標を投稿するためのルーティングを設定
   get 'goals/new' => 'goals#new'
   # 投稿した目標を保存するためのルーティングを設定
   post 'goals' => 'goals#create'
   # トップ画面のためのルーティングを設定
   root 'goals#top'
+  # 投稿した目標を削除するルーティングを設定
+  delete 'goals/:id' => 'goals#destroy'
+  
+  
   # サイトの説明の画面を表示させるためのルーティング
   get 'pages' => "pages#index"
+  
+  # マイページを表示させるためのルーティング
+  get 'users/:id' => 'users#show'
   
   
 end
