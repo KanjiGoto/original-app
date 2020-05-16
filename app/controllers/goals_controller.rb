cdc557cc8dfed16127308c11dea1c25f90d1cb76
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-16 07:47:53 +0000

    削除を実行するためのdestroyアクションを定義

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -7,7 +7,6 @@ class GoalsController < ApplicationController
     # アソシエーションを利用しているため、goalsテーブルから呼び出すのと同時にusersテーブルからニックネームを呼び出すというn+1問題が発生している
     # n+1問題を解消するために、includesメソッドを利用して、goalsテーブルのレコードを取得するのと同時にusersテーブルのレコードも同時に取得可能になっている
     @goals = Goal.all.includes(:user)
-    
   end
   
   def new
@@ -15,6 +14,13 @@ class GoalsController < ApplicationController
   
   def create
     Goal.create(goal: goal_params[:goal], reason: goal_params[:reason], future: goal_params[:future], user_id: current_user.id)
+  end
+  
+  def destroy
+    goal = Goal.find(params[:id])
+    if goal.user_id == current_user.id
+      goal.destroy
+    end
   end
   
   
