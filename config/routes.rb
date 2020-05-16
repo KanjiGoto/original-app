e1e16c91620eae8560370e68286884b40a80e772
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-16 07:52:50 +0000

    投稿した目標を編集するためのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   
   # 投稿した目標を削除するルーティングを設定
   delete 'goals/:id' => 'goals#destroy'
+  # 投稿した目標の詳細を編集するためのルーティングを設定
+  get 'goals/:id' => 'goals#edit'
   
   
   # サイトの説明の画面を表示させるためのルーティング
@@ -20,6 +22,8 @@ Rails.application.routes.draw do
   
   # マイページを表示させるためのルーティング
   get 'users/:id' => 'users#show'
+  
+
   
   
 end
