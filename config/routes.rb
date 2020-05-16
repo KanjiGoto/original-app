6b4850de30f44aed8b094c8d33c273436d7d6008
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-16 00:28:36 +0000

    目標の一覧の画面（index.html.erb）に投稿ボタンを追加した。それに伴っていくつかのファイルを編集した

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # 一覧表示のためのルーティングを設定
-  get 'goals/index' => 'goals#index'
+  get 'index' => 'goals#index'
   # 目標を投稿するためのルーティングを設定
   get 'goals/new' => 'goals#new'
   # 投稿した目標を保存するためのルーティングを設定
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # トップ画面のためのルーティングを設定
   root 'goals#top'
   # サイトの説明の画面を表示させるためのルーティング
-  get 'pages/index' => "pages#index"
+  get 'pages' => "pages#index"
   
   
 end
